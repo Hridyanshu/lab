3752a221cd5bd9090158e6bbe0cb49752019d8d5
Hridyanshu <hridyanshusharma@gmail.com>
2019-01-17 11:07:17 +0530

    Calculator 1

---
+++ calculator.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+class calculator
+{
+	public:
+	int choice;
+	float a,b;
+	float add(float a,float b);
+	float sub(float a,float b);
+	float product(float a,float b);
+	float division(float a,float b);	
+};
+int main()
+{
+	return 0;
+}
