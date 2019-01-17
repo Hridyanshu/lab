901493f4112cb8707c76c2b35b8db8d1625745d3
Hridyanshu <hridyanshusharma@gmail.com>
2019-01-17 11:24:30 +0530

    Calculator 2

--- calculator.cpp
+++ calculator.cpp
@@ -1,9 +1,8 @@
 #include<iostream>
+using namespace std;
 class calculator
 {
 	public:
-	int choice;
-	float a,b;
 	float add(float a,float b);
 	float sub(float a,float b);
 	float product(float a,float b);
@@ -11,5 +10,22 @@ class calculator
 };
 int main()
 {
+	int choice;
+	float a,b;
+	cout<<"Calculator program:"<<endl<<"1.Addition"<<endl<<"2.Subtraction"<<endl<<"3.Multiplication"<<endl<<"4.Division"<<endl;
+	cout<<"Enter your choice";
+	cin>>choice;
+	switch (choice)
+	{
+		case 1: add(a,b);
+		break;
+		case 2: sub(a,b);
+		break;
+		case 3: product(a,b);
+		break;
+		case 4: division(a,b);
+		break;
+		default: cout<<"Program terminated due to invalid choice."<<endl; 
+	}
 	return 0;
 }
