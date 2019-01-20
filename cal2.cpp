d33b4b8fd2bc3d9b7306c06e2feb46333a9b998e
Hridyanshu <hridyanshusharma@gmail.com>
2019-01-21 01:44:13 +0530

    Calculator2

---
+++ cal2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+class calculator
+{
+        public:
+                void sum();
+                void diff();
+                void product();
+                void division();
+};
+int main()
+{
+        float a,b;
+        int choice;
+        cout<<"1.Addition"<<endl<<"2.Subtraction"<<endl<<"3.Multiplication"<<endl<<"4.Division"<<endl<<"Please enter your choice"<<endl;
+}
+
