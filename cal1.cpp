6816a241be86b9f9301d26c7559af8ef15bbc212
Hridyanshu <hridyanshusharma@gmail.com>
2019-01-21 01:11:05 +0530

    Calculator1

---
+++ cal1.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+class calculator
+{
+        public:
+                void sum();
+                void diff();
+                void product();
+                void division();
+};
